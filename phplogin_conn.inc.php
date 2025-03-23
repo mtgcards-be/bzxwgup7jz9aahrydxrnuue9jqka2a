8c3f2d2758b6d3f07ebda4b4355dfe47844f0d01
Sven <sven.buyle@gmail.com>
2025-03-23 09:49:32 +0100

    Update phplogin_conn.inc.php

--- phplogin_conn.inc.php
+++ phplogin_conn.inc.php
@@ -1,13 +1,5 @@
 <?php
 
-// Edit the MySQL database details below
-/*
-	$DATABASE_HOST = 'localhost';
-	$DATABASE_USER = 'mtgcards_bephplogin';
-	$DATABASE_PASS = '4NC/P(Sbq5?f@&3~'; // 
-	$DATABASE_NAME = 'mtgcards_bephplogin';
-*/
-
 // LOCALHOST !!
 
 $DATABASE_HOST = 'localhost';
